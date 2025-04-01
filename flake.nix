8778a8f2ca7d78825dfa8e8fa9112102c7dd709e
Narthana Epa <narthana.epa@gmail.com>
2025-04-01 21:49:12 +1100

    Remove clutter from flake.nix

--- flake.nix
+++ flake.nix
@@ -34,20 +34,13 @@
 
         src = lib.cleanSourceWith { src = craneLib.path ./.; filter = fixturesOrCargo; };
 
-        # Common arguments can be set here to avoid repeating them later
         commonArgs = {
           inherit src;
           strictDeps = true;
 
-          buildInputs = [
-            # Add additional build inputs here
-          ] ++ lib.optionals pkgs.stdenv.isDarwin [
-            # Additional darwin specific inputs can be set here
+          buildInputs = lib.optionals pkgs.stdenv.isDarwin [
             pkgs.libiconv
           ];
-
-          # Additional environment variables can be set directly
-          # MY_CUSTOM_VAR = "some value";
         };
 
         craneLibLLvmTools = craneLib.overrideToolchain
@@ -57,27 +50,17 @@
             "rustc"
           ]);
 
-        # Build *just* the cargo dependencies, so we can reuse
-        # all of that work (e.g. via cachix) when running in CI
         cargoArtifacts = craneLib.buildDepsOnly commonArgs;
 
-        # Build the actual crate itself, reusing the dependency
-        # artifacts from above.
         riceware = craneLib.buildPackage (commonArgs // {
           inherit cargoArtifacts;
+          doCheck = false;
         });
       in
       {
         checks = {
-          # Build the crate as part of `nix flake check` for convenience
           inherit riceware;
 
-          # Run clippy (and deny all warnings) on the crate source,
-          # again, reusing the dependency artifacts from above.
-          #
-          # Note that this is done as a separate derivation so that
-          # we can block the CI if there are issues here, but not
-          # prevent downstream consumers from building our crate by itself.
           riceware-clippy = craneLib.cargoClippy (commonArgs // {
             inherit cargoArtifacts;
             cargoClippyExtraArgs = "--all-targets -- --deny warnings --deny clippy::pedantic --deny clippy::all";
@@ -87,18 +70,14 @@
             inherit cargoArtifacts;
           });
 
-          # Check formatting
           riceware-fmt = craneLib.cargoFmt {
             inherit src;
           };
 
           riceware-toml-fmt = craneLib.taploFmt {
             src = pkgs.lib.sources.sourceFilesBySuffices src [ ".toml" ];
-            # taplo arguments can be further customized below as needed
-            # taploExtraArgs = "--config ./taplo.toml";
           };
 
-          # Audit dependencies
           riceware-audit = craneLib.cargoAudit {
             inherit src advisory-db;
           };
@@ -108,9 +87,6 @@
             inherit src;
           };
 
-          # Run tests with cargo-nextest
-          # Consider setting `doCheck = false` on `riceware` if you do not want
-          # the tests to run twice
           riceware-nextest = craneLib.cargoNextest (commonArgs // {
             inherit cargoArtifacts;
             partitions = 1;
@@ -132,15 +108,10 @@
         };
 
         devShells.default = craneLib.devShell {
-          # Inherit inputs from checks.
           checks = self.checks.${system};
 
-          # Additional dev-shell environment variables can be set directly
-          # MY_CUSTOM_DEVELOPMENT_VAR = "something else";
-
-          # Extra inputs can be added here; cargo and rustc are provided by default.
           packages = [
-            # pkgs.ripgrep
+            pkgs.taplo
           ];
         };
       });
