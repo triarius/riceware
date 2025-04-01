c8de841c3ad55eb42d8168d363dfde446948871a
Narthana Epa <narthana.epa@gmail.com>
2025-04-01 21:52:57 +1100

    Update flake formatting

--- flake.nix
+++ flake.nix
@@ -20,99 +20,105 @@
     };
   };
 
-  outputs = { self, nixpkgs, crane, fenix, flake-utils, advisory-db, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
+  outputs = { self
+  , nixpkgs
+  , crane
+  , fenix
+  , flake-utils
+  , advisory-db
+  , ...
+  }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
 
-        inherit (pkgs) lib;
+      inherit (pkgs) lib;
 
-        craneLib = crane.mkLib pkgs;
+      craneLib = crane.mkLib pkgs;
 
-        fixturesFilter = path: _type: builtins.match ".*/fixtures/.*" path != null;
-        fixturesOrCargo = path: type: (fixturesFilter path type) || (craneLib.filterCargoSources path type);
+      fixturesFilter = path: _type: builtins.match ".*/fixtures/.*" path != null;
+      fixturesOrCargo = path: type: (fixturesFilter path type) || (craneLib.filterCargoSources path type);
 
-        src = lib.cleanSourceWith { src = craneLib.path ./.; filter = fixturesOrCargo; };
+      src = lib.cleanSourceWith { src = craneLib.path ./.; filter = fixturesOrCargo; };
 
-        commonArgs = {
-          inherit src;
-          strictDeps = true;
+      commonArgs = {
+        inherit src;
+        strictDeps = true;
 
-          buildInputs = lib.optionals pkgs.stdenv.isDarwin [
-            pkgs.libiconv
-          ];
-        };
+        buildInputs = lib.optionals pkgs.stdenv.isDarwin [
+          pkgs.libiconv
+        ];
+      };
 
-        craneLibLLvmTools = craneLib.overrideToolchain
-          (fenix.packages.${system}.complete.withComponents [
-            "cargo"
-            "llvm-tools"
-            "rustc"
-          ]);
+      craneLibLLvmTools = craneLib.overrideToolchain
+        (fenix.packages.${system}.complete.withComponents [
+          "cargo"
+          "llvm-tools"
+          "rustc"
+        ]);
 
-        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+      cargoArtifacts = craneLib.buildDepsOnly commonArgs;
 
-        riceware = craneLib.buildPackage (commonArgs // {
-          inherit cargoArtifacts;
-          doCheck = false;
-        });
-      in
-      {
-        checks = {
-          inherit riceware;
-
-          riceware-clippy = craneLib.cargoClippy (commonArgs // {
-            inherit cargoArtifacts;
-            cargoClippyExtraArgs = "--all-targets -- --deny warnings --deny clippy::pedantic --deny clippy::all";
-          });
-
-          riceware-doc = craneLib.cargoDoc (commonArgs // {
-            inherit cargoArtifacts;
-          });
-
-          riceware-fmt = craneLib.cargoFmt {
-            inherit src;
-          };
-
-          riceware-toml-fmt = craneLib.taploFmt {
-            src = pkgs.lib.sources.sourceFilesBySuffices src [ ".toml" ];
-          };
-
-          riceware-audit = craneLib.cargoAudit {
-            inherit src advisory-db;
-          };
-
-          # Audit licenses
-          riceware-deny = craneLib.cargoDeny {
-            inherit src;
-          };
-
-          riceware-nextest = craneLib.cargoNextest (commonArgs // {
-            inherit cargoArtifacts;
-            partitions = 1;
-            partitionType = "count";
-            cargoNextestPartitionsExtraArgs = "--no-tests=pass";
-          });
-        };
-
-        packages = {
-          default = riceware;
-        } // lib.optionalAttrs (!pkgs.stdenv.isDarwin) {
-          riceware-llvm-coverage = craneLibLLvmTools.cargoLlvmCov (commonArgs // {
-            inherit cargoArtifacts;
-          });
-        };
-
-        apps.default = flake-utils.lib.mkApp {
-          drv = riceware;
-        };
-
-        devShells.default = craneLib.devShell {
-          checks = self.checks.${system};
-
-          packages = [
-            pkgs.taplo
-          ];
-        };
+      riceware = craneLib.buildPackage (commonArgs // {
+        inherit cargoArtifacts;
+        doCheck = false;
       });
+    in {
+      checks = {
+        inherit riceware;
+
+        riceware-clippy = craneLib.cargoClippy (commonArgs // {
+          inherit cargoArtifacts;
+          cargoClippyExtraArgs = "--all-targets -- --deny warnings --deny clippy::pedantic --deny clippy::all";
+        });
+
+        riceware-doc = craneLib.cargoDoc (commonArgs // {
+          inherit cargoArtifacts;
+        });
+
+        riceware-fmt = craneLib.cargoFmt {
+          inherit src;
+        };
+
+        riceware-toml-fmt = craneLib.taploFmt {
+          src = pkgs.lib.sources.sourceFilesBySuffices src [ ".toml" ];
+        };
+
+        riceware-audit = craneLib.cargoAudit {
+          inherit src advisory-db;
+        };
+
+        # Audit licenses
+        riceware-deny = craneLib.cargoDeny {
+          inherit src;
+        };
+
+        riceware-nextest = craneLib.cargoNextest (commonArgs // {
+          inherit cargoArtifacts;
+          partitions = 1;
+          partitionType = "count";
+          cargoNextestPartitionsExtraArgs = "--no-tests=pass";
+        });
+      };
+
+      packages = {
+        default = riceware;
+      } // lib.optionalAttrs (!pkgs.stdenv.isDarwin) {
+        riceware-llvm-coverage = craneLibLLvmTools.cargoLlvmCov (commonArgs // {
+          inherit cargoArtifacts;
+        });
+      };
+
+      apps.default = flake-utils.lib.mkApp {
+        drv = riceware;
+      };
+
+      devShells.default = craneLib.devShell {
+        checks = self.checks.${system};
+
+        packages = [
+          pkgs.taplo
+        ];
+      };
+    }
+  );
 }
